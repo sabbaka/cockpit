1bb0fbca660c42904e252574972a13c3d506717d
Kyrylo Gliebov <kgliebov@redhat.com>
2017-11-27 17:02:50 +0100

    Add playback scaling
    Use css transform property

--- pkg/session_recording/player.jsx
+++ pkg/session_recording/player.jsx
@@ -464,6 +464,9 @@
             this.fastForwardToEnd = this.fastForwardToEnd.bind(this);
             this.skipFrame = this.skipFrame.bind(this);
             this.handleKeyDown = this.handleKeyDown.bind(this);
+            this.sync = this.sync.bind(this);
+            this.zoomIn = this.zoomIn.bind(this);
+            this.zoomOut = this.zoomOut.bind(this);
 
             this.state = {
                 cols:       80,
@@ -473,6 +476,7 @@
                 paused:     true,
                 /* Speed exponent */
                 speedExp:   0,
+                scale: 1,
             };
 
             /* Auto-loading buffer of recording's packets */
@@ -532,7 +536,8 @@
                 cols: this.state.cols,
                 rows: this.state.rows,
                 screenKeys: true,
-                useStyle: true
+                useStyle: true,
+                cursorBlink: false,
             });
 
             term.on('title', this.handleTitleChange);
@@ -591,6 +596,15 @@
             this.setState({ title: _("Player") + ": " + title });
         }
 
+        _transform() {
+            this.setState({ scale: 1});
+            var relation = Math.min(
+              630 / this.state.term.element.offsetWidth,
+              290 / this.state.term.element.offsetHeight
+            );
+            this.setState({ scale: relation});
+        }
+
         /* Synchronize playback */
         sync() {
             let locDelay;
@@ -620,6 +634,9 @@
 
                     /* Skip packets we don't output */
                     if (!pkt.is_io || !pkt.is_output) {
+                        this.setState({cols: pkt.width, rows: pkt.height});
+                        this.state.term.resize(pkt.width, pkt.height);
+                        this._transform();
                         continue;
                     }
 
@@ -728,6 +745,16 @@
             }
         }
 
+        zoomIn() {
+            let scale = this.state.scale;
+            this.setState({scale: scale + 0.1});
+        }
+
+        zoomOut() {
+            let scale = this.state.scale;
+            this.setState({scale: scale - 0.1});
+        }
+
         componentWillUpdate(nextProps, nextState) {
             /* If we changed pause state or speed exponent */
             if (nextState.paused != this.state.paused ||
@@ -758,6 +785,29 @@
                 speedStr = "";
             }
 
+            const style = {
+                "transform": "scale(" + this.state.scale + ") translate(-50%, -50%)",
+                "transform-origin": "top left",
+                "display": "inline-block",
+                "margin": "0 auto",
+                "position": "absolute",
+                "top": "50%",
+                "left": "50%",
+                // "transform": "translate(-50%, -50%)",
+            };
+
+            const scrollwrap = {
+                "min-width": "630px",
+                "height": "290px",
+                "background-color": "#f5f5f5",
+                "overflow": "hidden",
+                "position": "relative",
+            };
+
+            const to_right = {
+                "float": "right",
+            };
+
             // ensure react never reuses this div by keying it with the terminal widget
             return (
                 <div className="panel panel-default">
@@ -765,7 +815,9 @@
                         <span>{this.state.title}</span>
                     </div>
                     <div className="panel-body">
-                        <div ref="term" className="console-ct" key={this.state.term} />
+                        <div style={scrollwrap}>
+                            <div ref="term" className="console-ct" key={this.state.term} style={style} />
+                        </div>
                     </div>
                     <div className="panel-footer">
                         <button title="Play/Pause - Hotkey: p" type="button" ref="playbtn"
@@ -802,6 +854,10 @@
                             x2
                         </button>
                         <span>{speedStr}</span>
+                        <span style={to_right}>
+                            <button title="Zoom In" type="button" className="btn btn-default btn-lg" onClick={this.zoomIn}><i className="fa fa-search-plus" aria-hidden="true" /></button>
+                            <button title="Zoom Out" type="button" className="btn btn-default btn-lg" onClick={this.zoomOut}><i className="fa fa-search-minus" aria-hidden="true" /></button>
+                        </span>
                     </div>
                 </div>
             );
