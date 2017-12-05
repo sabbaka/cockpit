989fa486d6c33cf7e3f825599d695017433e2397
Kyrylo Gliebov <kgliebov@redhat.com>
2017-12-05 12:22:50 +0100

    [WIP] Add tlog config edit

---
+++ pkg/session_recording/config.jsx
@@ -0,0 +1,171 @@
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2017 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function() {
+    "use strict";
+
+    // let $ = require("jquery");
+    let cockpit = require("cockpit");
+    // let _ = cockpit.gettext;
+    let React = require("react");
+
+    let json = require('comment-json');
+
+    let Config = class extends React.Component {
+        constructor(props) {
+            super(props);
+            this.handleInputChange = this.handleInputChange.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+            this.setConfig = this.setConfig.bind(this);
+            this.prepareConfig = this.prepareConfig.bind(this);
+            this.file = null;
+            this.state = {
+                config: null,
+            }
+        }
+        /*
+            payload
+            log
+                input
+                output
+                window
+            limit
+                rate
+                burst
+                action
+            file
+                path
+            syslog
+                facility
+                priority
+            journal
+                priority
+                augment
+            writer
+                journal
+         */
+
+        handleInputChange(e){
+            const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+            const name = e.target.name;
+            const config = this.state.config;
+            config[name] = value;
+
+            this.forceUpdate();
+        }
+
+        prepareConfig() {
+            this.state.config.latency = parseInt(this.state.config.latency);
+        }
+
+        handleSubmit() {
+            this.prepareConfig();
+            this.file.replace(this.state.config).done( function() {
+                console.log('updated');
+            })
+                .fail( function(error) {
+                    console.log(error);
+                });
+            event.preventDefault();
+        }
+
+        setConfig(data) {
+            this.setState({config: data});
+        }
+
+        componentDidMount() {
+            let parseFunc = function(data) {
+                console.log(data);
+                // return data;
+                return json.parse(data, null, true);
+            };
+
+            let stringifyFunc = function(data) {
+                return json.stringify(data, null, true);
+            };
+            // needed for cockpit.file usage
+            let syntax_object = {
+                parse: parseFunc,
+                stringify: stringifyFunc,
+            };
+
+            this.file = cockpit.file("/etc/tlog/tlog-rec-session.conf", {
+                syntax: syntax_object,
+                // binary: boolean,
+                // max_read_size: int,
+                superuser: true,
+                // host: string
+            });
+
+            let promise = this.file.read();
+
+            promise.done(this.setConfig);
+
+            promise.fail(function(error) {
+                console.log(error);
+            });
+        }
+
+        render() {
+            if (this.state.config != null) {
+                return (
+                    <form onSubmit={this.handleSubmit}>
+                    <table className="info-table-ct col-sm-3">
+                        <tbody>
+                            <tr>
+                                <td><label htmlFor="shell">Shell</label></td>
+                                <td>
+                                    <input type="text" id="shell" name="shell" value={this.state.config.shell}
+                                           className="form-control" onChange={this.handleInputChange} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="notice">Notice</label></td>
+                                <td>
+                                    <input type="text" id="notice" name="notice" value={this.state.config.notice}
+                                           className="form-control" onChange={this.handleInputChange} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="latency">Latency</label></td>
+                                <td>
+                                    <input type="text" id="latency" name="latency" value={this.state.config.latency}
+                                           className="form-control" onChange={this.handleInputChange} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+
+                                </td>
+                                <td>
+                                    <button type="submit">Save</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </form>
+                );
+            } else {
+                return (<div></div>);
+            }
+        }
+    }
+
+    React.render(<Config />, document.getElementById('view'));
+}());
