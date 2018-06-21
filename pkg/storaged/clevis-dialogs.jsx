84a8d8a0384aa8e7cabe470ecc3fa23d1aaf1e6a
Marius Vollmer <mvollmer@redhat.com>
2018-06-21 15:47:04 +0200

    storage: Manage passphrases for LUKS volumes

--- pkg/storaged/clevis-dialogs.jsx
+++ pkg/storaged/clevis-dialogs.jsx
@@ -22,12 +22,13 @@ import React from "react";
 import sha1 from "js-sha1";
 import stable_stringify from "json-stable-stringify";
 
-import { dialog_open, TextInput, PassInput } from "./dialogx.jsx";
+import { dialog_open, SelectOne, TextInput, PassInput, Intermission } from "./dialogx.jsx";
 import { decode_filename } from "./utils.js";
 import { StorageButton } from "./storage-controls.jsx";
 
 import * as python from "python.jsx";
 import luksmeta_monitor_hack_py from "raw!./luksmeta-monitor-hack.py";
+import clevis_luks_passphrase_sh from "raw!./clevis-luks-passphrase.sh";
 
 const _ = cockpit.gettext;
 
@@ -94,29 +95,135 @@ function clevis_remove(block, key) {
                          { superuser: true, err: "message", pty: true });
 }
 
+function clevis_recover_passphrase(block) {
+    var dev = decode_filename(block.Device);
+    return cockpit.script(clevis_luks_passphrase_sh, [ dev ],
+                          { superuser: true, err: "message" })
+            .then(output => {
+                output = output.trim();
+                if (output == "")
+                    return cockpit.reject(_("No passphrase could be retrieved from keyservers. Please provide a existing passphrase."));
+                return output;
+            });
+}
+
+/* Passphrase operations
+ */
+
+function passphrase_add(block, new_passphrase, old_passphrase) {
+    var dev = decode_filename(block.Device);
+    return cockpit.spawn([ "cryptsetup", "luksAddKey", dev ],
+                         { superuser: true, err: "message" }).input(old_passphrase + "\n" + new_passphrase);
+}
+
+function passphrase_change(block, key, new_passphrase, old_passphrase) {
+    var dev = decode_filename(block.Device);
+    return cockpit.spawn([ "cryptsetup", "luksChangeKey", dev, "--key-slot", key.slot.toString() ],
+                         { superuser: true, err: "message" }).input(old_passphrase + "\n" + new_passphrase + "\n");
+}
+
+/* Generic slot operations
+ */
+
+function slot_remove(block, slot) {
+    var dev = decode_filename(block.Device);
+    return cockpit.spawn([ "cryptsetup", "luksKillSlot", "-q", dev, slot.toString() ],
+                         { superuser: true, err: "message", pty: true });
+}
+
 /* Dialogs
  */
 
-function add(client, block) {
-    edit(client, block, null);
+function existing_passphrase_fields() {
+    return [
+        Intermission("You need to provide a existing passphrase in order to make changes to the keys.  If you leave the field empty, a passphrase will be retrieved from a configured keyserver, if possible."),
+        PassInput("passphrase", _("Existing passphrase"), { })
+    ];
 }
 
-function edit(client, block, key) {
-    dialog_open({ Title: key ? _("Edit keyserver") : _("Add keyserver"),
+function get_existing_passphrase(block, vals) {
+    if (vals.passphrase)
+        return Promise.resolve(vals.passphrase);
+    else
+        return clevis_recover_passphrase(block);
+}
+
+function add_dialog(client, block) {
+    dialog_open({ Title: _("Add key slot"),
                   Fields: [
-                      TextInput("tang_url", _("Tang keyserver URL"),
-                                { validate: val => !val.length && _("Tang URL cannot be empty"),
-                                  value: key ? key.url : ""
+                      SelectOne("type", _("Slot type"),
+                                { value: "tang" },
+                                [ { value: "luks-passphrase", title: _("Passphrase") },
+                                    { value: "tang", title: _("Tang keyserver") }
+                                ]),
+                      PassInput("new_passphrase", _("New passphrase"),
+                                { visible: vals => vals.type == "luks-passphrase",
+                                  validate: val => !val.length && _("Passphrase cannot be empty")
                                 }),
-                      PassInput("passphrase", _("Existing passphrase"),
-                                { validate: val => !val.length && _("Passphrase cannot be empty"),
+                      PassInput("new_passphrase2", _("Confirm new passphrase"),
+                                { visible: vals => vals.type == "luks-passphrase",
+                                  validate: (val, vals) => vals.new_passphrase.length && vals.new_passphrase != val && _("Passphrases do not match")
+                                }),
+                      TextInput("tang_url", _("Tang keyserver URL"),
+                                { visible: vals => vals.type == "tang",
+                                  validate: val => !val.length && _("Tang URL cannot be empty")
+                                })
+                  ].concat(existing_passphrase_fields()),
+                  Action: {
+                      Title: _("Add"),
+                      action: function (vals) {
+                          return get_existing_passphrase(block, vals)
+                                  .then(passphrase => {
+                                      if (vals.type == "luks-passphrase") {
+                                          return passphrase_add(block, vals.new_passphrase, passphrase);
+                                      } else {
+                                          return get_tang_adv(vals.tang_url).then(function (adv) {
+                                              edit_tang_adv(client, block, null, vals.tang_url, adv, passphrase);
+                                          });
+                                      }
+                                  });
+                      }
+                  }
+    });
+}
+
+function edit_passphrase_dialog(block, key) {
+    dialog_open({ Title: _("Change passphrase"),
+                  Fields: [
+                      PassInput("old_passphrase", _("Old passphrase"),
+                                { validate: val => !val.length && _("Passphrase cannot be empty")
+                                }),
+                      PassInput("new_passphrase", _("New passphrase"),
+                                { validate: val => !val.length && _("Passphrase cannot be empty")
+                                }),
+                      PassInput("new_passphrase2", _("Confirm new passphrase"),
+                                { validate: (val, vals) => vals.new_passphrase.length && vals.new_passphrase != val && _("Passphrases do not match")
                                 })
                   ],
                   Action: {
-                      Title: key ? _("Apply") : _("Add"),
+                      Title: _("Apply"),
                       action: function (vals) {
-                          return get_tang_adv(vals.tang_url).then(function (adv) {
-                              edit_tang_adv(client, block, key, vals.tang_url, adv, vals.passphrase);
+                          return passphrase_change(block, key, vals.new_passphrase, vals.old_passphrase);
+                      }
+                  }
+    });
+}
+
+function edit_clevis_dialog(client, block, key) {
+    dialog_open({ Title: _("Edit tang keyserver"),
+                  Fields: [
+                      TextInput("tang_url", _("Tang keyserver URL"),
+                                { validate: val => !val.length && _("Tang URL cannot be empty"),
+                                  value: key.url
+                                })
+                  ].concat(existing_passphrase_fields()),
+                  Action: {
+                      Title: _("Apply"),
+                      action: function (vals) {
+                          return get_existing_passphrase(block, vals).then(passphrase => {
+                              return get_tang_adv(vals.tang_url).then(adv => {
+                                  edit_tang_adv(client, block, null, vals.tang_url, adv, passphrase);
+                              });
                           });
                       }
                   }
@@ -168,17 +275,30 @@ function verify_tang_adv(url, adv, title, extra, action_title, action) {
     });
 }
 
-function remove(client, block, key) {
-    dialog_open({ Title: _("Please confirm network keyserver removal"),
+function remove_passphrase_dialog(block, key) {
+    dialog_open({ Title: _("Remove passphrase"),
+                  Body: <p>{_("Removing a passphrase might prevent future unlocking of the volume and might thus cause data loss.")}</p>,
+                  Action: {
+                      DangerButton: true,
+                      Title: _("Remove passphrase"),
+                      action: function () {
+                          return slot_remove(block, key.slot);
+                      }
+                  }
+    });
+}
+
+function remove_clevis_dialog(client, block, key) {
+    dialog_open({ Title: _("Please confirm key slot removal"),
                   Body: (
                       <div>
-                          <p>{cockpit.format(_("The keyserver $0 will be removed."), key.url)}</p>
-                          <p>{_("Removing keyservers might prevent unattended booting.")}</p>
+                          <p>{_("Removing a key slot might prevent future unlocking of the volume and might thus cause data loss.")}</p>
+                          <p>{_("Also, removing keyservers might prevent unattended booting.")}</p>
                       </div>
                   ),
                   Action: {
                       DangerButton: true,
-                      Title: _("Remove keyserver"),
+                      Title: _("Remove key slot"),
                       action: function () {
                           return clevis_remove(block, key);
                       }
@@ -237,48 +357,79 @@ export class ClevisSlots extends React.Component {
                 var clevis = JSON.parse(slot.ClevisConfig.v);
                 if (clevis.pin && clevis.pin == "tang" && clevis.tang) {
                     return { slot: slot.Index.v,
+                             type: "tang",
                              url: clevis.tang.url
                     };
+                } else {
+                    return { slot: slot.Index.v,
+                             type: "unknown",
+                             pin: clevis.pin
+                    };
                 }
+            } else {
+                return { slot: slot.Index.v,
+                         type: "luks-passphrase"
+                };
             }
         }
 
-        var keys = this.state.slots.map(decode_clevis_slot).filter(k => !!k)
-                .sort((a, b) => a.url.localeCompare(b.url));
+        var keys = this.state.slots.map(decode_clevis_slot).filter(k => !!k);
+
+        function key_description(key) {
+            if (key.type == "tang")
+                return key.url;
+            else
+                return cockpit.format(_("Unknown slot type '$0'"), key.pin);
+        }
 
         var rows;
         if (keys.length == 0) {
-            rows = <tr><td className="text-center">{_("No network keyservers added")}</td></tr>;
+            rows = <tr><td className="text-center">{_("No key slots")}</td></tr>;
         } else {
-            rows = keys.map(key => {
-                if (key) {
-                    return (
-                        <tr>
-                            <td>{key.url}</td>
-                            <td className="text-right">
-                                <StorageButton onClick={() => edit(client, block, key)}>
-                                    <span className="pficon pficon-edit" />
-                                </StorageButton>
-                                { "\n" }
-                                <StorageButton onClick={() => remove(client, block, key)}>
-                                    <span className="fa fa-minus" />
-                                </StorageButton>
-                            </td>
-                        </tr>
-                    );
+            rows = [ ];
+
+            var add_row = (desc, edit, edit_excuse, remove) => {
+                rows.push(
+                    <tr>
+                        <td>{desc}</td>
+                        <td className="text-right">
+                            <StorageButton onClick={edit} excuse={edit_excuse}>
+                                <span className="pficon pficon-edit" />
+                            </StorageButton>
+                            { "\n" }
+                            <StorageButton onClick={remove}
+                                           excuse={keys.length == 1 ? _("The last key slot can not be removed") : null}>
+                                <span className="fa fa-minus" />
+                            </StorageButton>
+                        </td>
+                    </tr>
+                );
+            }
+
+            var passphrase_count = 0;
+            keys.sort((a, b) => a.slot - b.slot).forEach(key => {
+                if (key.type == "luks-passphrase") {
+                    add_row(cockpit.format(_("Passphrase $0"), ++passphrase_count),
+                            () => edit_passphrase_dialog(block, key), null,
+                            () => remove_passphrase_dialog(block, key));
+                } else {
+                    add_row(key_description(key),
+                            () => edit_clevis_dialog(client, block, key),
+                            key.type == "unknown" ? _("Key slots with unknown types can not be edited here") : null,
+                            () => remove_clevis_dialog(client, block, key))
                 }
-            })
+            });
         }
 
         return (
             <div className="panel panel-default">
                 <div className="panel-heading">
                     <div className="pull-right">
-                        <StorageButton onClick={() => add(client, block)}>
+                        <StorageButton onClick={() => add_dialog(client, block)}>
                             <span className="fa fa-plus" />
                         </StorageButton>
                     </div>
-                    {_("Network keyservers")}
+                    {_("Key slots")}
                 </div>
                 <table className="table">
                     <tbody> { rows } </tbody>
